aa39f1a540fd9a75c220ae9ff0a7e8c535c085e3
Bavly's <bavlye.shenouda95@gmail.com>
2019-08-14 17:04:41 +0200

    First Commit

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour {
+
+	//private TextAsset data =  
+    // Use this for initialization
+	void Start () {
+        loadDate();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void loadDate() {
+
+        Debug.Log("Start Fetch"); 
+        System.IO.StreamReader strReader = new System.IO.StreamReader("C:\\Users\\pc\\Desktop\\scores.csv");
+        bool endFile = false;
+        while (!endFile) { 
+            string data_string = strReader.ReadLine();
+            if (data_string == null) {
+                endFile = true;
+                break;
+            }
+            var data_values = data_string.Split(',');
+            Debug.Log(data_values[0].ToString()+ " : " +data_values[1].ToString()); 
+        }
+    }
+}
