6bc4395f269277e19345a60005938fd0852a95e3
Bavly's <bavlye.shenouda95@gmail.com>
2019-08-17 15:43:16 +0200

    Add arabic text

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using ArabicSupport;
+using UnityEngine.UI;
+using System.IO;
+using System.Text;
 
 public class Main : MonoBehaviour {
 
-	//private TextAsset data =  
+    public Text txt;
+
     // Use this for initialization
 	void Start () {
         loadDate();
@@ -17,8 +22,8 @@ public class Main : MonoBehaviour {
 
     void loadDate() {
 
-        Debug.Log("Start Fetch"); 
-        System.IO.StreamReader strReader = new System.IO.StreamReader("C:\\Users\\pc\\Desktop\\scores.csv");
+        Debug.Log("Start Fetch");
+        StreamReader strReader = new StreamReader("C:\\Users\\pc\\Desktop\\score.csv", Encoding.UTF8);
         bool endFile = false;
         while (!endFile) { 
             string data_string = strReader.ReadLine();
@@ -27,7 +32,8 @@ public class Main : MonoBehaviour {
                 break;
             }
             var data_values = data_string.Split(',');
-            Debug.Log(data_values[0].ToString()+ " : " +data_values[1].ToString()); 
+            txt.text = ArabicFixer.Fix(data_values[0].ToString());
+            Debug.Log(data_values[0].ToString() + " : " +data_values[1].ToString()); 
         }
     }
 }
