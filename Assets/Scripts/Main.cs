013f235d9b24c10935a4863009f28ec8c2ab5a12
Bavly's <bavlye.shenouda95@gmail.com>
2019-08-20 13:31:45 +0200

    Updated motion and images

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -1,17 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using ArabicSupport;
 using UnityEngine.UI;
 using System.IO;
 using System.Text;
 
 public class Main : MonoBehaviour {
 
-    public Text txt;
-
+    //public Text txt;
+    public GameObject titlePrefab;
+    private int rank;
     // Use this for initialization
 	void Start () {
+        rank = 1;
         loadDate();
 	}
 	
@@ -21,8 +22,7 @@ public class Main : MonoBehaviour {
 	}
 
     void loadDate() {
-
-        Debug.Log("Start Fetch");
+        //Debug.Log("Start Fetch");
         StreamReader strReader = new StreamReader("C:\\Users\\pc\\Desktop\\score.csv", Encoding.UTF8);
         bool endFile = false;
         while (!endFile) { 
@@ -32,8 +32,17 @@ public class Main : MonoBehaviour {
                 break;
             }
             var data_values = data_string.Split(',');
-            txt.text = ArabicFixer.Fix(data_values[0].ToString());
-            Debug.Log(data_values[0].ToString() + " : " +data_values[1].ToString()); 
+            //txt.text = ArabicFixer.Fix(data_values[0].ToString());
+            //Debug.Log(data_values[0].ToString() + " : " +data_values[1].ToString());
+            titlePrefab.GetComponent<tile>().animal = data_values[0].ToString();
+            titlePrefab.GetComponent<tile>().name = data_values[1].ToString();
+            titlePrefab.GetComponent<tile>().score = data_values[2].ToString();
+            titlePrefab.GetComponent<tile>().rank = rank.ToString();
+            float yLoc = (float)(-4 - ((rank - 1) * 0.9));
+            Vector3 loc = new Vector3(0, yLoc, 0);
+            Instantiate(titlePrefab, loc, Quaternion.identity);
+            rank++;
+
         }
     }
 }
---
+++ Assets/Scripts/tile.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using System.Text;
+using System.Collections;
+using ArabicSupport;
+
+
+public class tile : MonoBehaviour {
+
+    public string animal;
+    public string name;
+    public string score;
+    public string rank;
+    public GameObject pic;
+    public Text txt;
+    public Text rankTxt;
+	// Use this for initialization
+	void Start () {
+        Debug.Log(animal);
+        Material picMat = (Material)Resources.Load(animal, typeof(Material)) as Material;
+        Debug.Log(picMat);
+        pic.GetComponent<Renderer>().material = picMat;
+        txt.text = ArabicFixer.Fix(name + " : " + score);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(0, Time.deltaTime*0.5f ,0);
+	}
+}
